ed8edddb5ab3be1a0fd73a27be96cb03dc88219c
Bob Ambrose <bambrose24@gmail.com>
2021-10-27 19:16:02 -0400

    Updates

---
+++ precache-manifest.8990f48d4e0f0ac3b0e33d0d2441f6e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "873f0cdfedb9649d28f4ee4eae3d985c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5bf4bc6f22c75800aa92",
+    "url": "./static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bf850399f5ef97ea9e40",
+    "url": "./static/js/2.42282ae5.chunk.js"
+  },
+  {
+    "revision": "4844a3cf5a99d1261bd981ecae8befeb",
+    "url": "./static/js/2.42282ae5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d24846c1867efba612a9",
+    "url": "./static/js/3.e59578f2.chunk.js"
+  },
+  {
+    "revision": "5bf4bc6f22c75800aa92",
+    "url": "./static/js/main.ad5678d5.chunk.js"
+  },
+  {
+    "revision": "91082eddfcb0e0228aa3",
+    "url": "./static/js/runtime-main.71c7c5c1.js"
+  }
+]);
