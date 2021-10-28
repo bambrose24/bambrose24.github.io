98818437b4726eca6023fad14b532a1ce7c141c8
Bob Ambrose <bambrose24@gmail.com>
2021-10-27 20:46:58 -0400

    wording

--- personal-site/src/HomePage.js
+++ personal-site/src/HomePage.js
@@ -24,18 +24,19 @@ function HomePage() {
                 }} />
                 <Typography variant="h5" sx={{ textAlign: "center", fontWeight: "bold", marginBottom: "5px" }}>About</Typography>
                 <Typography variant="body1">
-                    I'm a senior software engineer who loves to think about people. I strive on building APIs and infrastructure that will scale, not just technically, but also in how it's used by fellow engineers. <b>Currently I'm on a "personal sabbatical" after resigning from Facebook.</b>
+                    I'm a senior software engineer who loves to think about people. I strive on building APIs and infrastructure that will scale, not just technically, but also in how it is used by fellow engineers. <b>Currently I'm on a "personal sabbatical" after resigning from Facebook.</b>
                     <br /><br />
                     I'm interested in making a positive difference in the world through technology while also having fun and not committing my entire self to work. If that sounds like you, please reach out to me at bambrose24 at gmail.
                 </Typography>
                 <Divider sx={{ marginTop: "20px", marginBottom: "20px" }} />
                 <Typography variant="body1">
-                    Ok, phew, glad that's over. I generally loathe the hustle-y nature of personal websites. Unfortunately they are great at being a one-stop-shop for your professional background. Although this one isn't that full, it's a good start as I prepare to look for what I'm going to do next.
+                    Ok, phew, glad that's over. I generally loathe the hustle-y nature of personal websites. Unfortunately, they are great at being a one-stop-shop for your professional background. Although this one isn't that full, it's a good start as I prepare to look for what's next.
                 </Typography>
                 <Divider sx={{ marginTop: "20px", marginBottom: "20px" }} />
                 <Typography variant="h5" sx={{ textAlign: "center", fontWeight: "bold", marginBottom: "5px" }}>Links</Typography>
                 <Typography variant="body1">
                     <ul>
+                        <li><a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1q1Ad-jtYGRnoxciTZuCgiYDJDkqfBlDT/view?usp=sharing">Resume</a></li>
                         <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/bambrose24">Github</a></li>
                         <li><a target="_blank" rel="noopener noreferrer" href="https://linkedin.com/in/ambroserobert">LinkedIn</a></li>
                         <li><a target="_blank" rel="noopener noreferrer" href="https://play-funtime.com">Funtime</a> (a NFL pickem pool built and designed by me)</li>
